10f629a5ea9b0dad5607a3826a8a8207b9735b6b
sohilraza01 <rsohil204@gmail.com>
2025-11-30 23:15:24 +0530

    final

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -7,22 +7,22 @@ const Projects = () => {
       <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>A small section of recent <br /> <span className='text-cyan-300'>projects</span></h1>
       <div className="w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
         <div data-aos='fade-up' data-aos-anchor-placement='top-center' daa-aos-delay='0'>
-            <Image src='/Images/p1.jpg' alt='img' width={800} height={650} className='rounded-lg' />
+            <Image src='Images/p1.jpg' alt='img' width={800} height={650} className='rounded-lg' />
             <h1 className='mt-4 text-xl sm:text-2xl font-semibold text-white'>Modern Finance Dashboard UI</h1>
             <h1 className='pt-2 font-medium text-white/80'>Apps , UI/UX</h1>
         </div>
          <div data-aos='fade-up' data-aos-anchor-placement='top-center' daa-aos-delay='100'>
-            <Image src='/Images/p2.jpg' alt='img' width={800} height={650} className='rounded-lg' />
+            <Image src='Images/p2.jpg' alt='img' width={800} height={650} className='rounded-lg' />
             <h1 className='mt-4 text-xl sm:text-2xl font-semibold text-white'>Portfolio website for creative</h1>
             <h1 className='pt-2 font-medium text-white/80'>Branding , Motion</h1>
         </div>
          <div data-aos='fade-up' data-aos-anchor-placement='top-center' daa-aos-delay='200'>
-            <Image src='/images/p3.jpg' alt='img' width={800} height={650} className='rounded-lg' />
+            <Image src='Images/p3.jpg' alt='img' width={800} height={650} className='rounded-lg' />
             <h1 className='mt-4 text-xl sm:text-2xl font-semibold text-white'>Brand Identity for startup</h1>
             <h1 className='pt-2 font-medium text-white/80'>Branding , UI/UX</h1>
         </div>
          <div data-aos='fade-up' data-aos-anchor-placement='top-center' daa-aos-delay='300'>
-            <Image src='/images/p4.jpg' alt='img' width={800} height={650} className='rounded-lg' />
+            <Image src='Images/p4.jpg' alt='img' width={800} height={650} className='rounded-lg' />
             <h1 className='mt-4 text-xl sm:text-2xl font-semibold text-white'>SaaS Dashboard Development</h1>
             <h1 className='pt-2 font-medium text-white/80'>Apps , UI/UX</h1>
         </div>
